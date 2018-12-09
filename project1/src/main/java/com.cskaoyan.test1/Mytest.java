09768c828b6be9230f6289fd6c5c9881aa5b6984
San-Qian <295201440@qq.com>
2018-12-09 21:37:36 +0800

    this is  7

--- project1/src/main/java/com.cskaoyan.test1/Mytest.java
+++ project1/src/main/java/com.cskaoyan.test1/Mytest.java
@@ -1,12 +1,19 @@
 package com.cskaoyan.test1;
 
 public class Mytest {
-    public void test1(){
+    public void test1() {
         System.out.println("test1");
         System.out.println("test3");
         System.out.println("test2");
     }
-    public void test65(){
+
+    public void test65() {
         System.out.println("test4564654");
+        System.out.println("test1");
+        System.out.println("test3");
+        System.out.println("test2");
     }
+
 }
+
+
